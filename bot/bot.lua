3e93af6b73eb417b8fe8e9b7e590514871a8aa47
CCAAHH <ccaahh430@gmail.com>
2016-01-22 20:47:59 +0000

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {118333567,75213323,139990182,121069994,171618507,121259485},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
